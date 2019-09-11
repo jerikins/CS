6b3eee591f4f1783b8e08ac59715f39c124eee8e
jerikins <55174805+jerikins@users.noreply.github.com>
2019-09-11 11:51:20 +0700

    Init and first app
    SortApp is first!

---
+++ cs/SortApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввод чисел
+            int[] nums = new int[7];
+            Console.WriteLine("Введите семь чисел");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{i + 1}-е число: ");
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            //Сортировка
+            int temp;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                for(int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            //Вывод
+            Console.WriteLine("Вывод отсортированного массива");
+            foreach (int i in nums)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
